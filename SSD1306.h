3ddb218daa8ddb948e08c84dd830e0b910a724ba
F1F7Y <64418963+F1F7Y@users.noreply.github.com>
2021-06-16 12:33:58 +0200

    Got rid of cplusplus check

--- SSD1306.h
+++ SSD1306.h
@@ -10,9 +10,6 @@
 #ifndef SSD1306_H_
 #define SSD1306_H_
 
-#ifdef __cplusplus
-extern "C"{
-#endif
 
 #define WIDTH 128
 #define HEIGHT 64
@@ -26,8 +23,5 @@ void SSD1306_data(uint8_t data);
 void SSD1306_set_col_address();
 void SSD1306_set_page_address();
 
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* SSD1306_H_ */
